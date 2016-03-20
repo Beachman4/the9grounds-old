e6879399e5527c9a9d351b6cf7d36d952dffef9e
Beachman4 <beachman4@hotmail.com>
2016-03-20 14:52:50 -0500

    lets catch some bots

--- app/Http/Controllers/BaseController.php
+++ app/Http/Controllers/BaseController.php
@@ -6,11 +6,19 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use DB;
 
 class BaseController extends Controller
 {
     public function index()
     {
+        if ($_SERVER['REMOTE_ADDR'] != '67.216.96.44' || $_SERVER['REMOTE_ADDR'] != '::1') {
+            DB::table('botcatcher')->insert([
+                'ip'    =>  $_SERVER['REMOTE_ADDR'],
+                'log'   =>  'BOTTTTTTT',
+            ]);
+        }
+
         return view('index');
     }
 
