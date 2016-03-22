27ed326d68fc491568858975dae25a7010c9ff72
Aylon Armstrong <aylon@groupm7.com>
2016-03-22 14:47:59 -0500

    Viewport issue

--- resources/views/master/master.blade.php
+++ resources/views/master/master.blade.php
@@ -1,6 +1,7 @@
 <html>
     <head>
         <title>The Nine Grounds</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="{{ URL::asset('assets/css/style.css') }}">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/foundation/6.1.2/foundation.min.css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
