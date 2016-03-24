2297d5cc45bae327a567e67bba1b2b8664c31ab0
Aylon Armstrong <aylon@groupm7.com>
2016-03-24 17:59:28 -0500

    Testing

--- app/Http/Controllers/BaseController.php
+++ app/Http/Controllers/BaseController.php
@@ -27,12 +27,12 @@ class BaseController extends Controller
             $message->to('beachman19@gmail.com');
             $message->subject('Testing');
         });*/
-        if (session()->has('user_id')) {
+        /*if (session()->has('user_id')) {
             $user = Users::find(session()->get('user_id'));
             //$this->dispatch(new SendTestEmail($user));
             $job = (new SendTestEmail($user))->delay(60 * 2);
             $this->dispatch($job);
-        }
+        }*/
 
         return view('index');
     }
